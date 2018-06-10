17658e9de59bff7cd9cccc7317005058e85ca5ad
Michael Huang <1525940982@qq.com>
2018-06-09 22:49:09 -0300

    JUnitTest1

---
+++ JUnitTest1.java
@@ -0,0 +1,34 @@
+package com.example.michael.assignment2;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+public class JUnitTest1 {
+    private validator v = new validator();
+
+    @Test
+    public void test1(){
+        assertFalse(v.PWformat("password"));
+    }
+
+    @Test
+    public void test2(){
+        assertTrue(v.PWformat("password!"));
+    }
+
+    @Test
+    public void test3(){
+        assertTrue(v.PWformat("PPASSWORD"));
+    }
+
+    @Test
+    public void test4(){
+        assertTrue(v.PWformat("ppassword"));
+    }
+    @Test
+    public void test5(){
+        assertTrue(v.PWformat("12343567777"));
+    }
+}
